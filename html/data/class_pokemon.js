21322b5c2dd6cb591fe1794465b078afd6ac22f8
Timéo <timeodoom@gmail.com>
2026-03-17 12:07:04 +0100

    Refactor move definitions to use ES6 exports and update test imports for consistency

--- html/data/class_pokemon.js
+++ html/data/class_pokemon.js
@@ -1,24 +1,23 @@
 import { Type } from "./class_type.js";
 import { Attack } from "./class_attack.js";
 
-class Pokemon {
-    static all_pokemons = {};
+export class Pokemon {
+  static all_pokemons = {};
 
-    constructor(id, name, types, attacks) {
-        this.id = id;
-        this.name = name;
-        this.types = types;
-        this.attacks = attacks;
-        Pokemon.all_pokemons[id] = this;
-    }
+  constructor(id, name, types, attacks) {
+    this.id = id;
+    this.name = name;
+    this.types = types;
+    this.attacks = attacks;
+    Pokemon.all_pokemons[id] = this;
+  }
 
-    toString() {
+  toString() {
+    return `${this.name} : #${this.id}, [${this.types.map((t) => t.name).join(", ")}], [STA: ${this.stats.sta}, ATK: ${this.stats.atk}, DEF: ${this.stats.def}], Rapides = [${this.attacks.fast.map((a) => a.name).join(", ")}], Chargées = [${this.attacks.charged.map((a) => a.name).join(", ")}]`;
+  }
 
-    }
-
-    getTypes() {
-
-    }
+  getTypes() {
+  }
 
     // Fonction getAttacks() renvoyant la liste des objets Attack (cf. class_attack.js) attribués à un Pokémon
 
