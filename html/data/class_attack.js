b76e5f3041580a34e55c6b99c5d30c5266ad1548
Maaxxiimee <maximebourbon562@gmail.com>
2026-03-17 11:25:18 +0100

    Classe attaque v1 ( test frecup données fast_moves.js )

---
+++ html/data/class_attack.js
@@ -0,0 +1,28 @@
+import "pokemon_moves.js";
+import "fast_moves.js";
+import "charged_moves.js";
+
+class Attack {
+
+    all_attacks = [];
+
+    constructor(id, name, type, power, delay) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.power = power;
+        this.delay = delay;
+    }
+
+    toString() {
+        return (this.name + " : #" + this.id + ", " + this.type + ", " + this.power + ", " + this.delay + "ms");
+    }
+
+    fill_attacks() {
+        attk = new Attack();
+        for (let attack of fast_moves) {
+            attk = Attack(attack["move_id"], attack["name"], attack["type"], attack["power"], attack["duration"]);
+            this.all_attacks[attack["move_id"]] = attk;
+        }
+    }
+}
