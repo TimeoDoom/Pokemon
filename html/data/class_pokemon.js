efb075b1410783df32bcb9758a7612d891db1edf
Timéo <timeodoom@gmail.com>
2026-03-18 13:47:17 +0100

    Refactor Pokemon class constructor to include stats and improve methods for clarity and efficiency

--- html/data/class_pokemon.js
+++ html/data/class_pokemon.js
@@ -4,49 +4,60 @@ import { Attack } from "./class_attack.js";
 export class Pokemon {
   static all_pokemons = {};
 
-  constructor(id, name, types, attacks) {
+  constructor(id, name, types, attacks, stats) {
     this.id = id;
     this.name = name;
-    this.types = types;
-    this.attacks = attacks;
+
+    this.types = types.map((t) => Type.all_types[t]);
+
+    this.attacks = {
+      fast: attacks.fast.map((id) => Attack.all_attacks[id]),
+      charged: attacks.charged.map((id) => Attack.all_attacks[id]),
+    };
+
+    this.stats = {
+      sta: stats.sta,
+      atk: stats.atk,
+      def: stats.def,
+    };
+
     Pokemon.all_pokemons[id] = this;
   }
 
   toString() {
-    return `${this.name} : #${this.id}, [${this.types.map((t) => t.name).join(", ")}], [STA: ${this.stats.sta}, ATK: ${this.stats.atk}, DEF: ${this.stats.def}], Rapides = [${this.attacks.fast.map((a) => a.name).join(", ")}], Chargées = [${this.attacks.charged.map((a) => a.name).join(", ")}]`;
+    return `${this.name} : #${this.id}, [${this.types
+      .map((t) => t.name)
+      .join(", ")}], [STA: ${this.stats.sta}, ATK: ${this.stats.atk}, DEF: ${
+      this.stats.def
+    }], Rapides = [${this.attacks.fast
+      .map((a) => a.name)
+      .join(", ")}], Chargées = [${this.attacks.charged
+      .map((a) => a.name)
+      .join(", ")}]`;
   }
 
   getTypes() {
-    let types = [];
-
-    for (let type of this.types) {
-      types.push(Type.all_types[type]);
-    }
-
-    return types;
+    return this.types;
   }
 
   getAttacks() {
-    let attacks = [];
-
-    for (let move of this.attacks.fast) {
-      attacks.push(Attack.all_attacks[move]);
-    }
-
-    for (let move of this.attacks.charged) {
-      attacks.push(Attack.all_attacks[move]);
-    }
-
-    return attacks;
+    return [...this.attacks.fast, ...this.attacks.charged];
   }
 
   static fill_pokemons(pokemon_data) {
+    Pokemon.all_pokemons = {};
+
     for (let pokemon of pokemon_data) {
       new Pokemon(
         pokemon["id"],
         pokemon["name"],
         pokemon["type"],
         pokemon["attacks"],
+        {
+          sta: pokemon["base_stamina"],
+          atk: pokemon["base_attack"],
+          def: pokemon["base_defense"],
+        },
       );
     }
   }
