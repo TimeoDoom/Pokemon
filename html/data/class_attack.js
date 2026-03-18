df021ff3e43e2eb58d4d229cb04f487079e4c447
Timéo <timeodoom@gmail.com>
2026-03-18 13:49:43 +0100

    Refactor Attack class to streamline fill_attacks method and improve code clarity

--- html/data/class_attack.js
+++ html/data/class_attack.js
@@ -1,52 +1,41 @@
-// Importation des données requises
-
 import { pokemon_moves } from "./pokemon_moves.js";
 import { fast_moves } from "./fast_moves.js";
 import { charged_moves } from "./charged_moves.js";
 
-// Création de la classe Attacks repertoriant toutes les attaques des Pokémons
-
 export class Attack {
+  // Index de toutes les attaques, accessible par move_id.
+  static all_attacks = {};
 
-    // Objet qui va stocker la totalité des attaques sous formes d'objet indexés sur l'id de l'attaque
+  constructor(id, name, type, power, duration) {
+    this.id = id;
+    this.name = name;
+    this.type = type;
+    this.power = power;
+    this.duration = duration;
+  }
 
-    static all_attacks = {};
+  toString() {
+    return `${this.name} : #${this.id}, ${this.type}, ${this.power}, ${this.duration}ms`;
+  }
 
-    // Constructeur de chaque attaque
+  static fill_attacks() {
+    // Réinitialise le cache avant de le reconstruire.
+    Attack.all_attacks = {};
 
-    constructor(id, name, type, power, delay) {
-        this.id = id;
-        this.name = name;
-        this.type = type;
-        this.power = power;
-        this.delay = delay;
+    // Regroupe toutes les sources d'attaques dans une seule collection.
+    const all = [...fast_moves, ...charged_moves, ...pokemon_moves];
+
+    for (let attack of all) {
+      const attk = new Attack(
+        attack["move_id"],
+        attack["name"],
+        attack["type"],
+        attack["power"],
+        attack["duration"],
+      );
+
+      // Stocke l'attaque par son identifiant pour un accès direct.
+      Attack.all_attacks[attack["move_id"]] = attk;
     }
-
-    // Fonction toString() permettant de retourner une chaine de charactère contenant les informations relatives à une attaque
-
-    toString() {
-        return (this.name + " : #" + this.id + ", " + this.type + ", " + this.power + ", " + this.delay + "ms");
-    }
-
-    // Fonction fill_attacks() permettant de remplir le tableau de donnée all_attacks[] en récupérant
-    // les données dans les différents fichiers importés au débuts
-
-    static fill_attacks() {
-        let attk = new Attack();
-
-        // Récupération des attaques dans fast_moves.js
-
-        for (let attack of fast_moves) {
-            attk = new Attack(attack["move_id"], attack["name"], attack["type"], attack["power"], attack["duration"]);
-            this.all_attacks[attack["move_id"]] = attk;
-        }
-
-        // Récupération des attaques dans charges_moves.js
-        for (let attack of charged_moves) {
-            attk = new Attack(attack["move_id"], attack["name"], attack["type"], attack["power"], attack["duration"]);
-            this.all_attacks[attack["move_id"]] = attk;
-        }
-
-        return this.all_attacks;
-    }
+  }
 }
