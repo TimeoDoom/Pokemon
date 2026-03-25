fd56e13523469aa1c6a647cf3bd3c1f6048a2f52
Timéo <timeodoom@gmail.com>
2026-03-25 11:09:45 +0100

    s

--- html/test/test.js
+++ html/test/test.js
@@ -6,101 +6,111 @@ Type.fill_types();
 Attack.fill_attacks();
 Pokemon.fill_pokemons();
 
+// Question 1
 function getPokemonsByType(typeName) {
-    const result = [];
+  const result = [];
 
-    if (!typeName) {
-      console.log("Aucun type fourni.");
-      return result;
-    }
-
-    for (const pokemon of Object.values(Pokemon.all_pokemons)) {
-      const types = pokemon.getTypes();
-
-      if (
-        types.some((t) => t && t.name.toLowerCase() === typeName.toLowerCase())
-      ) {
-        result.push(pokemon);
-      }
-    }
-
-    console.log(`Pokemons de type ${typeName} (${result.length}) :`);
-    result.forEach((pokemon) => {
-      console.log(`- ${pokemon.name}`);
-    });
-
+  if (!typeName) {
+    console.log("Aucun type fourni.");
     return result;
   }
 
-function getPokemonsByAttack(attackName) {
-    const result = [];
-    const attack = Attack.getAttackByName(attackName);
+  for (const pokemon of Object.values(Pokemon.all_pokemons)) {
+    const types = pokemon.getTypes();
 
-    if (!attack) {
-      console.log(`Aucune attaque trouvée pour '${attackName}'.`);
-      return result;
+    if (
+      types.some((t) => t && t.name.toLowerCase() === typeName.toLowerCase())
+    ) {
+      result.push(pokemon);
     }
-
-    for (const pokemon of Object.values(Pokemon.all_pokemons)) {
-      const allAttacks = pokemon.getAttacks();
-
-      if (allAttacks.some((a) => a && a.id === attack.id)) {
-        result.push(pokemon);
-      }
-    }
-
-    console.log(`Pokemons avec l'attaque ${attackName} (${result.length}) :`);
-    result.forEach((pokemon) => {
-      console.log(`- ${pokemon.name}`);
-    });
-
-    return result;
   }
 
-function getAttacksByType(typeName) {
-    const result = [];
-    const type = Type.all_types[typeName];
+  console.log(`Pokemons de type ${typeName} (${result.length}) :`);
+  result.forEach((pokemon) => {
+    console.log(`- ${pokemon.name}`);
+  });
 
-    if (!type) {
-      console.log(`Aucun type trouvé pour '${typeName}'.`);
-      return result;
-    }
-
-    for (const attack of Object.values(Attack.all_attacks)) {
-      if (attack.type === type.name) {
-        result.push(attack);
-      }
-    }
-
-    console.log(`Attaques de type ${typeName} (${result.lenght})`);
-    result.forEach((attack) => {
-      console.log(`- ${attack.name}`);
-    });
-  }
-
-function sortPokemonByTypeThenName() {
-    const result = Object.values(Pokemon.all_pokemons).sort((a, b) => {
-      const aTypes = a.getTypes().map((t) => t.name.toLowerCase()).join(", ");
-      const bTypes = b.getTypes().map((t) => t.name.toLowerCase()).join(", ");
-
-      if (aTypes < bTypes) return -1;
-      if (aTypes > bTypes) return 1;
-
-      const aName = a.name.toLowerCase();
-      const bName = b.name.toLowerCase();
-
-      if (aName < bName) return -1;
-      if (aName > bName) return 1;
-      return 0;
-    });
-    return result;
+  return result;
 }
 
-function fastFight(pokemonNameA, pokemonNameB) {
-  
+// Question 2
+function getPokemonsByAttack(attackName) {
+  const result = [];
+  const attack = Attack.getAttackByName(attackName);
 
+  if (!attack) {
+    console.log(`Aucune attaque trouvée pour '${attackName}'.`);
+    return result;
+  }
+
+  for (const pokemon of Object.values(Pokemon.all_pokemons)) {
+    const allAttacks = pokemon.getAttacks();
+
+    if (allAttacks.some((a) => a && a.id === attack.id)) {
+      result.push(pokemon);
+    }
+  }
+
+  console.log(`Pokemons avec l'attaque ${attackName} (${result.length}) :`);
+  result.forEach((pokemon) => {
+    console.log(`- ${pokemon.name}`);
+  });
+
+  return result;
+}
+
+// Question 3
+function getAttacksByType(typeName) {
+  const result = [];
+  const type = Type.all_types[typeName];
+
+  if (!type) {
+    console.log(`Aucun type trouvé pour '${typeName}'.`);
+    return result;
+  }
+
+  for (const attack of Object.values(Attack.all_attacks)) {
+    if (attack.type === type.name) {
+      result.push(attack);
+    }
+  }
+
+  console.log(`Attaques de type ${typeName} (${result.lenght})`);
+  result.forEach((attack) => {
+    console.log(`- ${attack.name}`);
+  });
+}
+
+// Question 4
+function sortPokemonByTypeThenName() {
+  const result = Object.values(Pokemon.all_pokemons).sort((a, b) => {
+    const aTypes = a
+      .getTypes()
+      .map((t) => t.name.toLowerCase())
+      .join(", ");
+    const bTypes = b
+      .getTypes()
+      .map((t) => t.name.toLowerCase())
+      .join(", ");
+
+    if (aTypes < bTypes) return -1;
+    if (aTypes > bTypes) return 1;
+
+    const aName = a.name.toLowerCase();
+    const bName = b.name.toLowerCase();
+
+    if (aName < bName) return -1;
+    if (aName > bName) return 1;
+    return 0;
+  });
+  return result;
+}
+
+// Question 7
+function fastFight(pokemonNameA, pokemonNameB) {
   return x;
 }
+
 
 // Q1. Fonction getPokemonsByType(typeName)
 console.log(getPokemonsByType("Grass"));
@@ -125,4 +135,3 @@ let print = true;
 Pokemon.getBestFastAttacksForEnemy(print, "Bulbasaur");
 
 // Q7. Fonction fastFight(pokemonNameA, pokemonNameB)
-
